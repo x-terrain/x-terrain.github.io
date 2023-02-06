5164dd6de3d2c20fb8e59bf53769962b4eae962b
stc <stc@binaura.net>
2023-02-06 13:08:15 +0100

    not resize

--- src/main.js
+++ src/main.js
@@ -136,7 +136,7 @@ function mousePressed() {
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    //resizeCanvas(windowWidth, windowHeight);
 }
 
 function fetchToken() {
